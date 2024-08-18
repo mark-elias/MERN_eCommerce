e763659bb10e7cbf8f7393ddb01e5dea1d31f623
mark-elias <mark7elias@gmail.com>
2024-08-18 05:35:54 -0700

    setup server boilerplate in index.js

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,2 +1,16 @@
-console.log("hello world");
-console.log("hiiiii");
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+
+const app = express();
+
+// everytime we get data to our endpoint,
+// we want it to be in json format
+app.use(express.json());
+// allows us to access our api from our React app
+app.use(cors());
+
+//make connection to our mongoose database
+mongoose.connect();
+
+app.listen(3001, () => console.log("server started >_<"));
