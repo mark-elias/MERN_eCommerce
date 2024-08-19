2e750396eb00ea76feb96e7b4b2c04d84d614222
mark-elias <mark7elias@gmail.com>
2024-08-18 20:46:44 -0700

    connected to MongoDB cluster

--- server/src/index.ts
+++ server/src/index.ts
@@ -10,7 +10,19 @@ app.use(express.json());
 // allows us to access our api from our React app
 app.use(cors());
 
+// mongodb+srv://mark7elias:<password>@cluster0.39b8e.mongodb.net/
+
+// AXw7eWAPd
+const uri = "mongodb+srv://mark7elias:AXw7eWAPd@cluster0.39b8e.mongodb.net/";
+
 //make connection to our mongoose database
-mongoose.connect();
+mongoose
+  .connect(uri)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((error) => {
+    console.error("Error connecting to MongoDB:", error);
+  });
 
 app.listen(3001, () => console.log("server started >_<"));
