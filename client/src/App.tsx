1423ce48461ae0c87b1322adabd1f079bbcaad9b
mark-elias <mark7elias@gmail.com>
2024-08-24 10:58:11 -0700

    basic routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,10 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<NavBar></NavBar>}></Route>
+        <Route path="/"></Route>
         <Route path="/auth"></Route>
         <Route path="/checkout" />
         <Route path="/purchased-items" />
