f185074c2ca1a4151536c965ddc7b3acb0d32708
mark-elias <mark7elias@gmail.com>
2024-08-25 05:04:53 -0700

    implemented react-hook-form for Login and Register

--- client/src/components/Login.tsx
+++ client/src/components/Login.tsx
@@ -1,20 +1,38 @@
+import { FieldValues, useForm } from "react-hook-form";
+
 function Login() {
+  // useForm is a hook
+  const { register, handleSubmit } = useForm();
+  // submitting form
+  function onSubmit(data: FieldValues) {
+    console.log(data);
+  }
   return (
     <div className="form-container">
       <h1 className="text-center text-2xl">Login</h1>
       <p className="text-center">Enter your username and password to login</p>
-      <form className="form-input-section">
+      <form className="form-input-section" onSubmit={handleSubmit(onSubmit)}>
         <div>
           <label htmlFor="username" className="block">
             Username
           </label>
-          <input id="username" type="text" className="custom-input" />
+          <input
+            id="username"
+            type="text"
+            className="custom-input"
+            {...register("username")}
+          />
         </div>
         <div>
           <label htmlFor="password" className="block">
             Password
           </label>
-          <input id="password" type="password" className="custom-input" />
+          <input
+            id="password"
+            type="password"
+            className="custom-input"
+            {...register("password")}
+          />
         </div>
         <div className="flex justify-center">
           <button type="submit" className="custom-button-primary">
--- client/src/components/Register.tsx
+++ client/src/components/Register.tsx
@@ -1,34 +1,48 @@
-import { useForm } from "react-hook-form";
+import { FieldValues, useForm } from "react-hook-form";
 
 function Register() {
   //useForm is a hook
-  const { register } = useForm();
+  const { register, handleSubmit } = useForm();
+  // submitting the form
+  function onSubmit(data: FieldValues) {
+    console.log(data);
+  }
 
   return (
     <>
-        <div className="form-container">
-          <h1 className="text-center text-2xl">Register</h1>
-          <p className="text-center">Set a username and password to register</p>
-          <form className="form-input-section">
-            <div>
-              <label htmlFor="username" className="block">
-                Username
-              </label>
-              <input id="username" type="text" className="custom-input" />
-            </div>
-            <div>
-              <label htmlFor="password" className="block">
-                Set a password
-              </label>
-              <input id="password" type="password" className="custom-input" />
-            </div>
-            <div className="flex justify-center">
-              <button type="submit" className="custom-button-primary">
-                Register
-              </button>
-            </div>
-          </form>
-        </div>
+      <div className="form-container">
+        <h1 className="text-center text-2xl">Register</h1>
+        <p className="text-center">Set a username and password to register</p>
+        <form className="form-input-section" onSubmit={handleSubmit(onSubmit)}>
+          <div>
+            <label htmlFor="username" className="block">
+              Username
+            </label>
+            <input
+              id="username"
+              type="text"
+              className="custom-input"
+              {...register("username")}
+            />
+          </div>
+          <div>
+            <label htmlFor="password" className="block">
+              Set a password
+            </label>
+            <input
+              id="password"
+              type="password"
+              className="custom-input"
+              {...register("password")}
+            />
+          </div>
+          <div className="flex justify-center">
+            <button type="submit" className="custom-button-primary">
+              Register
+            </button>
+          </div>
+        </form>
+      </div>
     </>
   );
 }
