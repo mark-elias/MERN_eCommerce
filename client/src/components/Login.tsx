44b7b7077fbbe106ba3fc04e6ac78a88c31b52bc
mark-elias <mark7elias@gmail.com>
2024-08-25 09:19:43 -0700

    working Login on FrontEnd

--- client/src/components/Login.tsx
+++ client/src/components/Login.tsx
@@ -1,7 +1,10 @@
-import { FieldValues, useForm } from "react-hook-form";
-import { isValid, z } from "zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
-
+import axios from "axios";
+import { userErrors } from "../errors";
+import { useCookies } from "react-cookie";
+import { useNavigate } from "react-router-dom";
 // Validation
 const schema = z.object({
   username: z.string().min(3),
@@ -11,15 +14,47 @@ const schema = z.object({
 type FormData = z.infer<typeof schema>;
 
 function Login() {
+  const navigate = useNavigate();
+  const [_, setCookies] = useCookies(["access_token"]);
   // useForm is a hook
   const {
     register,
     handleSubmit,
-    formState: { errors, isValid },
+    formState: { errors },
   } = useForm<FormData>({ resolver: zodResolver(schema) });
   // submitting form
-  function onSubmit(data: FieldValues) {
+  function onSubmit(data: FormData) {
     console.log(data);
+
+    axios
+      .post("http://localhost:3001/user/login", {
+        username: data.username,
+        password: data.password,
+      })
+      .then((result) => {
+        // Assuming the token is returned in result.data.token
+        setCookies("access_token", result.data.token);
+        console.log("Login successful, token saved in cookies.");
+        localStorage.setItem("userID", result.data.userID);
+
+        // ✅ if login is succesful, send use to shop page
+        navigate("/");
+      })
+      .catch((err) => {
+        console.log("ERROR SECTION");
+        console.log(err.response.data.type);
+
+        switch (err.response.data.type) {
+          case userErrors.NO_USER_FOUND:
+            alert(userErrors.NO_USER_FOUND);
+            break;
+          case userErrors.WRONG_CREDENTIALS:
+            alert(userErrors.WRONG_CREDENTIALS);
+            break;
+          default:
+            alert("something went wrong");
+        }
+      });
   }
   return (
     <div className="form-container">
